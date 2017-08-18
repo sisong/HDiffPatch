aaf94e8bfffc89089be5661f95095fba589aac94
sisong <sisong@gmail.com>
2017-08-18 20:31:38 +0800

    添加支持bz2的流式压缩demo

--- compress_plugin_demo.h
+++ compress_plugin_demo.h
@@ -37,6 +37,7 @@
 
 #define kCompressBufSize (1024*16)
 #define _compress_error_return(result,err) { printf("\n%s\n",err); return result; }
+
 #ifdef  _CompressPlugin_zlib
 #include "zlib.h" // http://zlib.net/  https://github.com/madler/zlib
     static const char*  _zlib_stream_compressType(const hdiff_TStreamCompress* compressPlugin){
@@ -79,6 +80,7 @@
                                     hdiff_compressHandle compressHandle,
                                     const unsigned char* in_part_data,const unsigned char* in_part_data_end){
         int is_eof=(in_part_data==0)&&(in_part_data==in_part_data_end);
+        int is_stream_end=0;
         _zlib_stream_compress_t* self=(_zlib_stream_compress_t*)compressHandle;
         assert((in_part_data<in_part_data_end)||is_eof);
         self->c_stream.next_in = (Bytef*)in_part_data;
@@ -87,6 +89,7 @@
             if (self->c_stream.avail_out==kCompressBufSize){
                 if (is_eof){
                     int ret=deflate(&self->c_stream,Z_FINISH);
+                    is_stream_end= (ret==Z_STREAM_END);
                     if ((ret!=Z_STREAM_END)&&(ret!=Z_OK))
                         _compress_error_return(-1,"_zlib_stream_compress_part() deflate() Z_FINISH error!");
                 }else{
@@ -106,6 +109,7 @@
             self->writePos+=(size_t)writed;
             self->c_stream.next_out = (Bytef*)self->code_buf;
             self->c_stream.avail_out = kCompressBufSize;
+            if (is_stream_end) break;  //end loop
         }
         return (long)(in_part_data_end-in_part_data);
     }
@@ -144,9 +148,12 @@ static hdiff_TStreamCompress zlibStreamCompressPlugin={_zlib_stream_compressType
     
 #ifdef  _CompressPlugin_bz2
 #include "bzlib.h" // http://www.bzip.org/
-    static const char*  _bz2_compressType(const hdiff_TCompress* compressPlugin){
+    static const char*  _bz2_stream_compressType(const hdiff_TStreamCompress* compressPlugin){
         static const char* kCompressType="bz2";
         return kCompressType;
+    }
+    static const char*  _bz2_compressType(const hdiff_TCompress* compressPlugin){
+        return _bz2_stream_compressType(0);
     }
     static size_t  _bz2_maxCompressedSize(const hdiff_TCompress* compressPlugin,size_t dataSize){
         return dataSize*5/4+16*1024;
@@ -159,13 +166,83 @@ static hdiff_TStreamCompress zlibStreamCompressPlugin={_zlib_stream_compressType
         int ret = BZ2_bzBuffToBuffCompress((char*)out_code,&destLen,
                                            (char *)data,(unsigned int)(data_end-data),
                                            9, 0, 0);
-        if(ret != BZ_OK){
-            std::cout <<"\nBZ2_bzBuffToBuffCompress error\n";
-            return 0;
-        }
+        if(ret != BZ_OK)
+            _compress_error_return(0,"_bz2_compress() BZ2_bzBuffToBuffCompress() error!");
         return destLen;
     }
+
+    struct _bz2_stream_compress_t{
+        bz_stream                  c_stream;
+        const hdiff_TStreamOutput* out_code;
+        hpatch_StreamPos_t         writePos;
+        unsigned char   code_buf[kCompressBufSize];
+    };
+    static hdiff_compressHandle _bz2_stream_open(struct hdiff_TStreamCompress* compressPlugin,
+                                                 const hdiff_TStreamOutput* out_code){
+        _bz2_stream_compress_t* self=(_bz2_stream_compress_t*)malloc(sizeof(_bz2_stream_compress_t));
+        if (!self) _compress_error_return(0,"_bz2_stream_open() memory alloc error!");
+        memset(self,0,sizeof(_bz2_stream_compress_t)-kCompressBufSize);
+        self->out_code=out_code;
+        self->writePos=0;
+        self->c_stream.next_out = (char*)self->code_buf;
+        self->c_stream.avail_out = kCompressBufSize;
+        int ret = BZ2_bzCompressInit(&self->c_stream, 9, 0, 0);
+        if(ret != BZ_OK){
+            free(self); self=0;
+            _compress_error_return(0,"_bz2_stream_open() BZ2_bzCompressInit() error!");
+        }
+        return self;
+    }
+    long _bz2_stream_compress_part(const struct hdiff_TStreamCompress* compressPlugin,
+                                   hdiff_compressHandle compressHandle,
+                                   const unsigned char* in_part_data,const unsigned char* in_part_data_end){
+        int is_eof=(in_part_data==0)&&(in_part_data==in_part_data_end);
+        int is_stream_end=0;
+        _bz2_stream_compress_t* self=(_bz2_stream_compress_t*)compressHandle;
+        assert((in_part_data<in_part_data_end)||is_eof);
+        self->c_stream.next_in = (char*)in_part_data;
+        self->c_stream.avail_in = (unsigned int)(in_part_data_end-in_part_data);
+        while (true) {
+            if (self->c_stream.avail_out==kCompressBufSize){
+                if (is_eof){
+                    int ret=BZ2_bzCompress(&self->c_stream,BZ_FINISH);
+                    is_stream_end= (ret==BZ_STREAM_END);
+                    if ((ret!=BZ_STREAM_END)&&(ret!=BZ_FINISH_OK))
+                        _compress_error_return(-1,"_bz2_stream_compress_part() BZ2_bzCompress() BZ_FINISH error!");
+                }else{
+                    if (self->c_stream.avail_in>0){
+                        int ret=BZ2_bzCompress(&self->c_stream,BZ_RUN);
+                        if (ret!=BZ_RUN_OK)
+                            _compress_error_return(-1,"_bz2_stream_compress_part() BZ2_bzCompress() error!");
+                    }
+                }
+            }
+            long write_len=(long)(kCompressBufSize-self->c_stream.avail_out);
+            if (write_len==0) break; //end loop
+            long writed=self->out_code->write(self->out_code->streamHandle,self->writePos,
+                                              self->code_buf,self->code_buf+write_len);
+            if(writed!=write_len)
+                _compress_error_return(-1,"_bz2_stream_compress_part() write stream error!");
+            self->writePos+=(size_t)writed;
+            self->c_stream.next_out = (char*)self->code_buf;
+            self->c_stream.avail_out = kCompressBufSize;
+            if (is_stream_end) break;  //end loop
+        }
+        return (long)(in_part_data_end-in_part_data);
+    }
+    static hpatch_StreamPos_t _bz2_stream_close(struct hdiff_TStreamCompress* compressPlugin,
+                                                hdiff_compressHandle compressHandle){
+        _bz2_stream_compress_t* self=(_bz2_stream_compress_t*)compressHandle;
+        int is_error=((0-0)!=_bz2_stream_compress_part(compressPlugin,self,0,0));
+        int ret = BZ2_bzCompressEnd(&self->c_stream);
+        if (ret != BZ_OK) is_error=1;
+        hpatch_StreamPos_t codeSize=self->writePos;
+        if (self) free(self);
+        return is_error?0:codeSize;
+    }
     static hdiff_TCompress bz2CompressPlugin={_bz2_compressType,_bz2_maxCompressedSize,_bz2_compress};
+    static hdiff_TStreamCompress bz2StreamCompressPlugin={_bz2_stream_compressType,_bz2_stream_open,
+                                                          _bz2_stream_close,_bz2_stream_compress_part};
 #endif//_CompressPlugin_bz2
     
 #ifdef  _CompressPlugin_lzma
