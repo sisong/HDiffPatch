4f211a1f4cfc6e197f431ceee280e8ecb033c224
sisong <sisong@gmail.com>
2019-01-07 14:25:20 +0800

    调整 patch时 激活CacheOld功能的内存下限 策略

--- libHDiffPatch/HPatch/patch.c
+++ libHDiffPatch/HPatch/patch.c
@@ -1593,8 +1593,12 @@ hpatch_BOOL _patch_cache(hpatch_TCovers** out_covers,
                          TByte** ptemp_cache,TByte** ptemp_cache_end,hpatch_BOOL* out_isReadError){
     const size_t kMinCacheSize=hpatch_kStreamCacheSize*_kCacheCount;
 #if (_IS_NEED_CACHE_OLD_BY_COVERS)
-    const size_t kBestACacheSize=128*1024;   //内存足够时比较好的hpatch_kStreamCacheSize值;
-    const size_t kActiveCacheOldMemorySize=(1<<(20+3)); //激活CacheOld功能的内存下限,>7*kBestACacheSize;
+    const size_t kBestACacheSize=64*1024;   //内存足够时比较好的hpatch_kStreamCacheSize值;
+    const size_t _kMinActiveCacheSize=(1<<23);
+    size_t kActiveCacheOldMemorySize = //尝试激活CacheOld功能的内存下限;
+        kBestACacheSize*kBestACacheSize*2+newDataSize/8;
+    if (kActiveCacheOldMemorySize>_kMinActiveCacheSize)
+        kActiveCacheOldMemorySize=_kMinActiveCacheSize;
 #endif //_IS_NEED_CACHE_OLD_BY_COVERS
     const hpatch_TStreamInput* oldData=*poldData;
     TByte* temp_cache=*ptemp_cache;
