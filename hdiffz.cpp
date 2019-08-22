e631044162d1a49681cd45966baeae7247d67fb1
sisong <sisong@gmail.com>
2019-08-22 19:21:19 +0800

    fix build: make DIR_DIFF=0

--- hdiffz.cpp
+++ hdiffz.cpp
@@ -42,10 +42,9 @@
 #include "file_for_patch.h"
 #include "libHDiffPatch/HDiff/private_diff/mem_buf.h"
 
-#include "dirDiffPatch/dir_patch/dir_patch_types.h"
+#include "dirDiffPatch/dir_patch/dir_patch.h"
 #if (_IS_NEED_DIR_DIFF_PATCH)
 #include "dirDiffPatch/dir_diff/dir_diff.h"
-#include "dirDiffPatch/dir_patch/dir_patch.h"
 #endif
 
 #ifndef _IS_NEED_MAIN
