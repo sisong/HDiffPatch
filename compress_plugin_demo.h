b6342d9ed6cc08ad5f001ec455c9a3b1ee1b8383
sisong <sisong@gmail.com>
2022-11-03 21:24:59 +0800

    recode: update new version zstd compress API;

--- compress_plugin_demo.h
+++ compress_plugin_demo.h
@@ -892,9 +892,9 @@ int _default_setParallelThreadNumber(hdiff_TCompress* compressPlugin,int threadN
         ZSTD_inBuffer       s_input;
         ZSTD_outBuffer      s_output;
 #if (IS_REUSE_compress_handle)
-        static ZSTD_CStream*  s=0;
+        static ZSTD_CCtx*   s=0;
 #else
-        ZSTD_CStream*         s=0;
+        ZSTD_CCtx*          s=0;
 #endif
         hpatch_StreamPos_t  readFromPos=0;
         int                 outStream_isCanceled=0;
@@ -908,54 +908,57 @@ int _default_setParallelThreadNumber(hdiff_TCompress* compressPlugin,int threadN
         s_input.src=_temp_buf;
         s_output.dst=_temp_buf+s_input.size;
         
-        if (!s) s=ZSTD_createCStream();
-        if (!s) _compress_error_return("ZSTD_createCStream()");
-        ret=ZSTD_initCStream(s,plugin->compress_level);
-        if (ZSTD_isError(ret)) _compress_error_return("ZSTD_initCStream()");
-        
+        if (!s) s=ZSTD_createCCtx();
+        else ZSTD_CCtx_reset(s,ZSTD_reset_session_only);
+        if (!s) _compress_error_return("ZSTD_createCCtx()");
+        ret=ZSTD_CCtx_setParameter(s,ZSTD_c_compressionLevel,plugin->compress_level);
+        if (ZSTD_isError(ret)) _compress_error_return("ZSTD_CCtx_setParameter(,ZSTD_c_compressionLevel)");
+        ZSTD_CCtx_setPledgedSrcSize(s,in_data->streamSize);
         #define _ZSTD_WINDOWLOG_MIN 10
         dict_bits=plugin->dict_bits;
         while (((((hpatch_StreamPos_t)1)<<(dict_bits-1)) >= in_data->streamSize)
                 &&((dict_bits-1)>=_ZSTD_WINDOWLOG_MIN)) {
             --dict_bits;
         }
-        ret=ZSTD_CCtx_setParameter(s, ZSTD_c_windowLog,dict_bits);
+        ret=ZSTD_CCtx_setParameter(s,ZSTD_c_windowLog,dict_bits);
         if (ZSTD_isError(ret)) _compress_error_return("ZSTD_CCtx_setParameter(,ZSTD_c_windowLog)");
         if (plugin->thread_num>1){
             ret=ZSTD_CCtx_setParameter(s, ZSTD_c_nbWorkers,plugin->thread_num);
             //if (ZSTD_isError(ret)) printf("  (NOTICE: zstd unsupport multi-threading, warning.)\n");
         }
-        
-        while (readFromPos<in_data->streamSize) {
-            s_input.pos=0;
-            if (s_input.size>(in_data->streamSize-readFromPos))
-                s_input.size=(size_t)(in_data->streamSize-readFromPos);
-            if (!in_data->read(in_data,readFromPos,(unsigned char*)s_input.src,
-                               (unsigned char*)s_input.src+s_input.size))
-                _compress_error_return("in_data->read()");
-            readFromPos+=s_input.size;
-            while (s_input.pos<s_input.size) {
-                s_output.pos=0;
-                ret=ZSTD_compressStream(s,&s_output,&s_input);
-                if (ZSTD_isError(ret)) _compress_error_return("ZSTD_compressStream()");
-                if (s_output.pos>0){
-                    _stream_out_code_write(out_code,outStream_isCanceled,result,
-                                           (const unsigned char*)s_output.dst,s_output.pos);
-                }
+
+        for (;;){
+            if (readFromPos<in_data->streamSize){
+                s_input.pos=0;
+                if (s_input.size>(in_data->streamSize-readFromPos))
+                    s_input.size=(size_t)(in_data->streamSize-readFromPos);
+                if (!in_data->read(in_data,readFromPos,(unsigned char*)s_input.src,
+                                (unsigned char*)s_input.src+s_input.size))
+                    _compress_error_return("in_data->read()");
+                readFromPos+=s_input.size;
             }
-        }
-        while (1) {
-            s_output.pos=0;
-            ret=ZSTD_endStream(s,&s_output);
-            if (s_output.pos>0){
-                _stream_out_code_write(out_code,outStream_isCanceled,result,
-                                       (const unsigned char*)s_output.dst,s_output.pos);
+            {
+                const int lastChunk=(readFromPos==in_data->streamSize);
+                const ZSTD_EndDirective mode=lastChunk?ZSTD_e_end:ZSTD_e_continue;
+                int finished;
+                do {
+                    s_output.pos=0;
+                    ret=ZSTD_compressStream2(s,&s_output,&s_input,mode);
+                    if (ZSTD_isError(ret)) _compress_error_return("ZSTD_compressStream2()");
+                    if (s_output.pos>0){
+                        _stream_out_code_write(out_code,outStream_isCanceled,result,
+                                            (const unsigned char*)s_output.dst,s_output.pos);
+                    }
+                    finished=lastChunk?(ret==0):(s_input.pos==s_input.size);
+                } while (!finished);
+                //if (s_input.pos!=s_input.size) _compress_error_return("Impossible: zstd only returns 0 when the input is completely consumed!");
+                if (lastChunk)
+                    break;
             }
-            if (ret==0) break;
         }
     clear:
 #if (!IS_REUSE_compress_handle)
-        if (0!=ZSTD_freeCStream(s))
+        if (0!=ZSTD_freeCCtx(s))
         { result=kCompressFailResult; if (strlen(errAt)==0) errAt="ZSTD_freeCStream()"; }
 #endif
         _check_compress_result(result,outStream_isCanceled,"_zstd_compress()",errAt);
