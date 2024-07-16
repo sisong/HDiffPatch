043d7c7d9268e4cf7dbab244035ec90a89f8a8fe
sisong <housisong@hotmail.com>
2024-07-16 11:12:13 +0800

    support Android15 16KB PageSize

--- builds/android_ndk_jni_mk/Application.mk
+++ builds/android_ndk_jni_mk/Application.mk
@@ -3,5 +3,6 @@ APP_CFLAGS += -s -Wno-error=format-security
 APP_CFLAGS += -fvisibility=hidden -fvisibility-inlines-hidden
 APP_CFLAGS += -ffunction-sections -fdata-sections
 APP_LDFLAGS += -s -Wl,--gc-sections,--as-needed
+APP_LDFLAGS += -Wl,-z,max-page-size=16384
 APP_BUILD_SCRIPT := Android.mk
 APP_ABI := armeabi armeabi-v7a arm64-v8a x86 x86_64
