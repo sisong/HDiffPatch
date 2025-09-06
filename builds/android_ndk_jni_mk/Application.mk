5aca25cd6e1a1cb6c32c2c586d3d2460ec5ac6cb
sisong <sisong@gmail.com>
2025-09-06 20:44:07 +0800

    update min NDK version from r16b to r23c; ( android 14 API  to 16; remove .so for armeabi )

--- builds/android_ndk_jni_mk/Application.mk
+++ builds/android_ndk_jni_mk/Application.mk
@@ -1,8 +1,8 @@
-APP_PLATFORM := android-14
+APP_PLATFORM := android-16
 APP_CFLAGS += -s -Wno-error=format-security
 APP_CFLAGS += -fvisibility=hidden -fvisibility-inlines-hidden
 APP_CFLAGS += -ffunction-sections -fdata-sections
 APP_LDFLAGS += -s -Wl,--gc-sections,--as-needed
 APP_LDFLAGS += -Wl,-z,max-page-size=16384
 APP_BUILD_SCRIPT := Android.mk
-APP_ABI := arm64-v8a armeabi-v7a armeabi x86_64 x86
+APP_ABI := arm64-v8a armeabi-v7a x86_64 x86
